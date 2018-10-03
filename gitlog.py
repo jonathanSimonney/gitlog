674400cf168775ae8e6d11d7bdc6c977df2d3df3
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-10-04 00:08:00 +0200

    print commit date of commit.

--- gitlog.py
+++ gitlog.py
@@ -1,10 +1,16 @@
 import numpy as np
 import matplotlib as mlp
 import sys
+import time
 from git import Repo
 
 mlp.use('TkAgg')
 repo_path = a = sys.argv[1]
 
 repo = Repo(repo_path)
-print(repo)
+list_commits = list(repo.iter_commits())
+print(len(list_commits))
+
+for commit in list_commits:
+    print(commit.authored_date)
+    print(time.asctime(time.gmtime(commit.authored_date)))
