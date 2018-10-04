3ee518adda4d40e56dcaf7ce0532dbcef8249231
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-10-04 09:39:07 +0200

    dict with number of commit per date functional.

--- gitlog.py
+++ gitlog.py
@@ -5,12 +5,21 @@ import time
 from git import Repo
 
 mlp.use('TkAgg')
-repo_path = a = sys.argv[1]
+repo_path = sys.argv[1]
 
 repo = Repo(repo_path)
 list_commits = list(repo.iter_commits())
-print(len(list_commits))
+
+contribution_timeline_dict = {}
 
 for commit in list_commits:
-    print(commit.authored_date)
-    print(time.asctime(time.gmtime(commit.authored_date)))
+    print(commit.committed_date)
+    day_committed = (time.strftime("%a, %d %b %Y", time.gmtime(commit.committed_date)))
+    print(day_committed)
+    print(time.strftime("%a, %d %b %Y %H:%M", time.gmtime(commit.committed_date)))
+    if day_committed in contribution_timeline_dict:
+        contribution_timeline_dict[day_committed] += 1
+    else:
+        contribution_timeline_dict[day_committed] = 1
+
+print(contribution_timeline_dict)
