9178f8cb5f6cb4bb2bd52495cbe54e59a3a8a90c
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-10-09 16:01:07 +0200

    timeline almost completely alright.

--- gitlog.py
+++ gitlog.py
@@ -1,10 +1,53 @@
 import numpy as np
-import matplotlib as mlp
+import matplotlib
 import sys
 import time
 from git import Repo
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as pyp
 
-mlp.use('TkAgg')
+
+def generate_months_array():
+    twelve_months_array = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan']
+    ret = ["" for _ in range(53)]
+    acc = 0
+    month_number = 0
+    for i in range(len(ret)):
+        acc += 7
+        if (acc + 15) > 30:
+            ret[i] = twelve_months_array[month_number]
+            month_number += 1
+            acc -= 30
+    return ret
+
+
+def draw_picture(data_array, x_legend, y_legend, title):
+
+    fig, ax = pyp.subplots()
+    im = ax.imshow(data_array)
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(53))
+    ax.set_yticks(np.arange(7))
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(x_legend)
+    ax.set_yticklabels(y_legend)
+
+    # Rotate the tick labels and set their alignment.
+    # pyp.setp(ax.get_xticklabels(), rotation=90, ha="right",
+    #          rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    # for i in range(len(x_legend)):
+    #     for j in range(len(y_legend)):
+    #         text = ax.text(i, j, data_array[j, i],
+    #                        ha="center", va="center", color="w", )
+
+    ax.set_title(title)
+    fig.tight_layout()
+    pyp.show()
+
+
 repo_path = sys.argv[1]
 
 # we get the list of all the commits
@@ -13,11 +56,7 @@ list_commits = list(repo.iter_commits())
 
 # some configurations vars
 commit_intensity = 10
-
-# we prepare vars to be filled by our for loop
-# contribution_timeline_dict = {}
-scattered_plot_array = np.zeros((7, 24))
-timeline_array = np.zeros((53, 7))
+months_array = generate_months_array()
 day_number_correspondances = {
     "Sun": 0,
     "Mon": 1,
@@ -28,6 +67,11 @@ day_number_correspondances = {
     "Sat": 6,
 }
 
+# we prepare vars to be filled by our for loop
+# contribution_timeline_dict = {}
+scattered_plot_array = np.zeros((7, 24))
+timeline_array = np.zeros((7, 53))
+
 # we fill our dict and our ndarray
 for commit in list_commits:
     week_committed = int(time.strftime("%U", time.gmtime(commit.committed_date))) - 1
@@ -36,8 +80,14 @@ for commit in list_commits:
     day_int = day_number_correspondances[day]
     hour_int = int(time.strftime("%H", time.gmtime(commit.committed_date)))
     scattered_plot_array[day_int, hour_int] += 1
-    timeline_array[week_committed, day_int] += commit_intensity
+    timeline_array[day_int, week_committed] += commit_intensity
 
 # we print them
 print(timeline_array)
 print(scattered_plot_array)
+
+# pyp.imshow(timeline_array, origin="upper", extent=[0, 53, 0, 7],
+#                    interpolation="nearest")
+# pyp.draw()
+# pyp.show()
+draw_picture(timeline_array, months_array, ["", "Mon", "", "Wed", "", "Fri", ""], "Timeline.png")
